d3dcd6f6cc8195986863dbef6dc379ae152d824d
Sabra Kozlina <sjkozlina@gmail.com>
2024-12-13 08:36:44 -0700

    Added prices to orders

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,14 +1,14 @@
 export const OrderList = async () => {
-    const response = await fetch("http://localhost:8088/orders")
+    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await response.json()
 
     let ordersHTML = ""
     const ordersArray = orders.map(
         (order) => {
-            return `<div>Order #${order.id}</div>`
+            const orderPrice = order.metal.price + order.style.price + order.size.price
+            return `<div>Order #${order.id} cost $${orderPrice}</div>`
         }
     )
     ordersHTML += ordersArray.join("")
     return ordersHTML
 }
-// const orderPrice = order.metal.price + order.style.price + order.size.price
